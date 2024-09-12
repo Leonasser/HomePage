4a99fa7ff695b166b8b7074ce8d49108b4114a84
Leonasser <Leonardo.nasser2@yahoo.com.br>
2024-09-12 14:29:52 -0300

    Adicionar arquivos de projeto.

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 1337;
+
+// Middleware para servir arquivos estáticos
+app.use(express.static('Resources'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/Resources/Index/Index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
